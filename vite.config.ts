14a7cb0e63961bbb978d7dd93443aa252cc13de3
RYOTA-F <ry.fj0304@gmail.com>
2025-10-01 03:13:18 +0900

    :recycle: エントリファイル修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/guild-hp/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
