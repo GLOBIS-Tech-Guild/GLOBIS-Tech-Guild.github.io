73fbd1f6fc71dd2a7cba997a2405e418bbe8cfab
RYOTA-F <ry.fj0304@gmail.com>
2025-10-01 03:04:37 +0900

    :recycle: ベースURL変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/guild-hp/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
